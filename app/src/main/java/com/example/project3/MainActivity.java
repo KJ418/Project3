eb8e941776c04b410a63e796158bb1fe33b0a71c
ManaMythii <105987369+ManaMythii@users.noreply.github.com>
2024-11-23 18:43:36 -0600

    Updated files

--- app/src/main/java/com/example/project3/MainActivity.java
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.project3;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -23,18 +25,54 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+
     public void displayImage1(View v){
         ImageView image = findViewById(R.id.imageView);
+        Animation animFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+        Animation animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+
+        animFadeOut.reset();
+        image.clearAnimation();
+        image.startAnimation(animFadeOut);
+
         image.setImageResource(R.drawable.turtwig);
+
+        animFadeIn.reset();
+        image.clearAnimation();
+        image.startAnimation(animFadeIn);
+
     }
 
     public void displayImage2(View v){
         ImageView image = findViewById(R.id.imageView);
+        Animation animFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+        Animation animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+
+        animFadeOut.reset();
+        image.clearAnimation();
+        image.startAnimation(animFadeOut);
+
         image.setImageResource(R.drawable.chimchar);
+
+        animFadeIn.reset();
+        image.clearAnimation();
+        image.startAnimation(animFadeIn);
     }
 
     public void displayImage3(View v){
         ImageView image = findViewById(R.id.imageView);
+        Animation animFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+        Animation animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+
+        animFadeOut.reset();
+        image.clearAnimation();
+        image.startAnimation(animFadeOut);
+
         image.setImageResource(R.drawable.piplup);
+
+        animFadeIn.reset();
+        image.clearAnimation();
+        image.startAnimation(animFadeIn);
     }
 }
